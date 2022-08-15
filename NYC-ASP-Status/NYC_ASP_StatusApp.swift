2c15af5bab17e9bca0f1a902f8b33adb90ae39a5
mjs <jiasheng.ma@yahoo.com>
2022-08-15 14:48:41 -0400

    Initial Commit

---
+++ NYC-ASP-Status/NYC_ASP_StatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYC_ASP_StatusApp.swift
+//  NYC-ASP-Status
+//
+//  Created by Jia Sheng Ma on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct NYC_ASP_StatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NYC-ASP-StatusUITests/NYC_ASP_StatusUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NYC_ASP_StatusUITestsLaunchTests.swift
+//  NYC-ASP-StatusUITests
+//
+//  Created by Jia Sheng Ma on 8/15/22.
+//
+
+import XCTest
+
+class NYC_ASP_StatusUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
